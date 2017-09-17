4c93651543c7868ad685b669ddbb1bb7a6fe406e
mrmcwethy <mrmcwethy@yahoo.com>
2017-09-17 15:46:44 -0700

    UPdated to be the same as the current 2.0 demo main.py for gemma m0

--- main.py
+++ main.py
@@ -1,30 +1,30 @@
 # Gemma IO demo
-# Welcome to CircuitPython :)
+# Welcome to CircuitPython 2.0.0 :)
 
-from touchio import *
-from digitalio import *
-from analogio import *
-from board import *
+import board
+import time
+from digitalio import DigitalInOut, Direction, Pull
+from analogio import AnalogIn, AnalogOut
+from touchio import TouchIn
 from adafruit_hid.keyboard import Keyboard
 from adafruit_hid.keycode import Keycode
-import adafruit_dotstar
-import time
+import adafruit_dotstar as dotstar
 
 # One pixel connected internally!
-dot = adafruit_dotstar.DotStar(APA102_SCK, APA102_MOSI, 1, brightness=0.2)
+dot = dotstar.DotStar(board.APA102_SCK, board.APA102_MOSI, 1, brightness=0.2)
 
 # Built in red LED
-led = DigitalInOut(D13)
+led = DigitalInOut(board.D13)
 led.direction = Direction.OUTPUT
 
 # Analog output on A0
-aout = AnalogOut(A0)
+aout = AnalogOut(board.A0)
 
 # Analog input on A1
-analog1in = AnalogIn(A1)
+analog1in = AnalogIn(board.A1)
 
 # Capacitive touch on A2
-touch2 = TouchIn(A2)
+touch2 = TouchIn(board.A2)
 
 # Used if we do HID output, see below
 kbd = Keyboard()
@@ -40,17 +40,17 @@ def wheel(pos):
     # Input a value 0 to 255 to get a color value.
     # The colours are a transition r - g - b - back to r.
     if (pos < 0):
-        return (0, 0, 0)
+        return [0, 0, 0]
     if (pos > 255):
-        return (0, 0, 0)
+        return [0, 0, 0]
     if (pos < 85):
-        return (int(pos * 3), int(255 - (pos*3)), 0)
+        return [int(pos * 3), int(255 - (pos*3)), 0]
     elif (pos < 170):
         pos -= 85
-        return (int(255 - pos*3), 0, int(pos*3))
+        return [int(255 - pos*3), 0, int(pos*3)]
     else:
         pos -= 170
-        return (0, int(pos*3), int(255 - pos*3))
+        return [0, int(pos*3), int(255 - pos*3)]
 
 ######################### MAIN LOOP ##############################
 
